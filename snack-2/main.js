182dd36719437cd7326f48224043acfc5919b906
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-06 11:03:26 +0200

    rimosso elementi in eccesso ma problemi con .length

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,26 +4,19 @@
 
 // 1. creo array con all'intenro i numeri da andare a dividere in pari o dispari
 const numbers = Number[1,2,3,4,5,6,7,8,9,10];
+// 3. creo div per numeri dispari
+const boxDisp = document.querySelector('#dispari')
+// 4. creo div per numeri pari
+const boxPar = document.querySelector('#pari')
 
 // 2. sviluppo ciclo for per controllo numeri all'interno dell'array
 for(let i = 0; i < numbers.length; i++) {
+    const element = document.createElement("p")
+    console.log(numbers[i]);
     if ( numbers[i] % 2 === 1) {
-        // 3. creo div per numeri dispari
-        const boxDisp = document.querySelector('#dispari')
-        console.log(boxDisp)
-        boxDisp.classList.add("red")
-        const red = document.querySelector('.red')
-        red.innerText += Number(numbers);
-        console.log(red[numbers])
+        boxDisp.append(element)
 
     } else if (numbers[i] % 2 === 0) {
-        // 4. creo div per numeri pari
-        const boxPar = document.querySelector('#pari')
-        console.log(boxPar)
-        boxDisp.classList.add("green")
-        const green = document.querySelector('.green')
-        green.innerHTML += numbers;
+        boxPar.append(element)
     }
-    dispari.append(red);
-    pari.append(green);
 }
